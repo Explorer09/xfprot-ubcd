3f5d3204eb9069926ed7d4f82f08cae61812734e
Explorer09 <explorer09@gmail.com>
2012-11-30 02:12:47 +0800

    The modified locale files will now be used when building for Parted Magic.

--- ubcd/build-for-ubcd.sh
+++ ubcd/build-for-ubcd.sh
@@ -6,11 +6,22 @@
 # By Explorer <explorer09@gmail.com>
 
 cd `dirname $0`
-build_dir=`pwd`
+work_dir=`pwd`
 
-package_dir=$build_dir/xfprot-ubcd-pkg
+package_dir=$work_dir/xfprot-ubcd-pkg
 package_name=xfprot-2.4expl1-full.txz
 
+# For Parted Magic the locale directory structure is different, so we are
+# going to modify the locales here.
+cd ../xfprot
+mkdir -p po_bak
+mv po/*.po po_bak
+mv po/LINGUAS po_bak
+cd $work_dir
+cp po/*.po ../xfprot/po
+cp po/LINGUAS ../xfprot/po
+
+# Configure and make
 cd ../xfprot
 if [ -f Makefile ]; then
     make distclean
@@ -19,24 +30,39 @@ fi
 make
 mkdir -p $package_dir
 make DESTDIR=$package_dir install-strip
-cd $build_dir
 
-# Copy the config files for the root user
+cd $work_dir
+
+# Copy the config files for the root user into the package
 mkdir -p $package_dir/root/.xfprot
 cp root/xfprot/xfprot.config $package_dir/root/.xfprot/xfprot.config
 touch $package_dir/root/.xfprot/xfprot.no_root_warn
 touch $package_dir/root/.xfprot/xfprot.no_splash
 
 cd $package_dir
-find . | sort | sed -e '2,$ s/^\.\///g' -e '1 s/^\.$/\.\//' > ../xfprot-ubcd-pkg.list
 
+# Don't include these directories because they have no use in Parted Magic.
+rm -r usr/share/apps
+rm -r usr/share/man
+
+# Create the package.
 # Note: the --no-recursion works only for GNU tar.
+find . | sort | sed -e '2,$ s/^\.\///g' -e '1 s/^\.$/\.\//' > $work_dir/xfprot-ubcd-pkg.list
 tar -c -v --xz --no-recursion --files-from ../xfprot-ubcd-pkg.list -f $package_name
+mv $package_dir/$package_name $work_dir
 
-rm ../xfprot-ubcd-pkg.list
-cd $build_dir
-
-mv $package_dir/$package_name .
-
+# Clean up.
 rm -r $package_dir
+rm $work_dir/xfprot-ubcd-pkg.list
+cd $work_dir
+
+cd ../xfprot
+
+# Restore the po directory.
+rm po/*.po
+mv po/LINGUAS
+mv po_bak/*.po po
+mv po_bak/LINGUAS po
+rmdir po_bak
+
 
