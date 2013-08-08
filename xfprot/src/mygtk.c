e64d04799a558e8d73bc841214b3c31e016be87d
Explorer09 <explorer09@gmail.com>
2013-08-08 23:23:26 +0800

    mygtk.c: (GTK+ 3) Use gtk_progress_bar_set_inverted instead of gtk_progress_bar_set_orientation.

--- xfprot/src/mygtk.c
+++ xfprot/src/mygtk.c
@@ -1042,9 +1042,9 @@ static gboolean progress_timeout(gpointer data )
 		val += 0.05;
 		if (val > 1.0) {
 			val = 0.0;
-			gtk_progress_bar_set_orientation(GTK_PROGRESS_BAR (pdata->pbar), 
-					(gtk_progress_bar_get_orientation (GTK_PROGRESS_BAR (pdata->pbar)) == GTK_PROGRESS_RIGHT_TO_LEFT) ?
-					GTK_PROGRESS_LEFT_TO_RIGHT : GTK_PROGRESS_RIGHT_TO_LEFT);
+			gtk_progress_bar_set_inverted(GTK_PROGRESS_BAR (pdata->pbar), 
+					(gtk_progress_bar_get_inverted (GTK_PROGRESS_BAR (pdata->pbar)) == TRUE) ?
+					FALSE : TRUE);
 		}
 	}
 	
